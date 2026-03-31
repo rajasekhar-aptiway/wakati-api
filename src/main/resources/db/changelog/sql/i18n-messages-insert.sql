92b882319b26d870bd59d2beadfaff9eff3b79ff
rajasekhar-aptiway <rajasekhar.kasu@aptiway.com>
2026-03-31 16:10:40 +0530

    added liquibase

---
+++ src/main/resources/db/changelog/sql/i18n-messages-insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO wakati.i18n_messages(message_key,language,message) VALUES ('USER_ID_AND_OTP_REQUIRED','en','User ID and OTP are required'),	 ('USER_ID_REQUIRED','en','User ID is required'),
+
+('USER_NOT_FOUND','en','User not found'),
+
+('NO_OTP_FOUND','en','OTP not found'),
+
+('OTP_ALREADY_VERIFIED','en','OTP already verified'),
+
+('OTP_EXPIRED','en','OTP expired'),
+
+('INVALID_OTP','en','Invalid OTP'),
+
+('OTP_VERIFIED_SUCCESSFULLY','en','OTP verified successfully'),
+
+('MOBILE_ALREADY_REGISTERED','en','Mobile number already registered'),
+
+('EMAIL_ALREADY_REGISTERED','en','Email already registered'),
+
+('ID_NUMBER_ALREADY_USED_BY_2_USERS','en','ID number already used by two users'),
+
+('USER_REGISTERED_SUCCESSFULLY','en','User registered successfully'),
+
+('CUSTOMER_LIST_FETCHED_SUCCESSFULLY','en','Customer list fetched successfully'),
+
+('DEALER_LIST_FETCHED_SUCCESSFULLY','en','Dealer list fetched successfully'),
+
+('SUPER_DEALER_SUB_CATEGORY_NOT_FOUND','en','Super dealer sub-category not found'),
+
+('ACCOUNT_IS_NOT_ACTIVE','en','Account is not active'),
+
+('SESSION_INVALIDATED_PLEASE_LOGIN_AGAIN','en','Session invalidated please login again'),
+
+('INVALID_TOKEN','en','Invalid token'),
+
+('AUTHORIZATION_REQUIRED','en','Authorization is required'),
+
+('REFRESH_TOKEN_REQUIRED','en','Refresh token is required'),
+
+('INVALID_REFRESH_TOKEN','en','Invalid refresh token'),
+
+('REFRESH_TOKEN_REUSED','en','Refresh token already used'),
+
+('REFRESH_TOKEN_EXPIRED','en','Refresh token expired'),
+
+('INVALID_LOGIN_INPUT','en','Invalid login input'),
+
+('MOBILE_NOT_REGISTERED','en','Mobile number not registered'),
+
+('DEALER_NOT_ACTIVATED','en','Dealer not activated'),
+
+('INVALID_CREDENTIALS','en','Invalid credentials'),
+
+('LOGIN_SUCCESS','en','Login success'),
+
+('ERROR_WHILE_SENDING_EMAIL','en','Error while sending email'),
+('ERROR_WHILE_SENDING_SMS','en','Error while sending sms'),
+('INVALID_REQUEST','en','Invalid request'),
+
+('INVALID_CURRENT_PASSWORD','en','Invalid current password'),
+
+('PASSWORDS_MUST_NOT_BE_SAME','en','Passwords must not be same'),
+
+('PASSWORD_UPDATE_SUCCESS','en','Password updated successfully');
+ 
