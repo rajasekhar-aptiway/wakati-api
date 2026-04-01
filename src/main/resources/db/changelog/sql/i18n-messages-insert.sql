de973ebb32f5348a3b0301dde1900f17e28a6608
rajasekhar-aptiway <rajasekhar.kasu@aptiway.com>
2026-04-01 10:41:43 +0530

    liquibase changes

--- src/main/resources/db/changelog/sql/i18n-messages-insert.sql
+++ src/main/resources/db/changelog/sql/i18n-messages-insert.sql
@@ -60,5 +60,10 @@ INSERT INTO i18n_messages(message_key,language,message) VALUES ('USER_ID_AND_OTP
 
 ('PASSWORDS_MUST_NOT_BE_SAME','en','Passwords must not be same'),
 
-('PASSWORD_UPDATE_SUCCESS','en','Password updated successfully');
+('PASSWORD_UPDATE_SUCCESS','en','Password updated successfully'),
+
+('DOCUMENTS_UPLOADED_SUCCESSFULLY','en','Documents uploaded successfully'),
+('DOCUMENT_UPLOAD_FAILED','en','Documents upload failed'),
+('USER_ID_AND_PIN_REQUIRED','en','User ID and pin are required'),
+('PIN_SET_SUCCESSFULLY','en','Pin set successfully');
  
